426219c2818c06ce83160c504ddb1ce861155151
Fernando Valdez <fernando.valdez@ipointsystems.com>
2016-03-12 17:33:30 -0600

    changed gradle config

--- ScorePoint/app/src/main/java/com/ipointsystems/scorepoint/DashboardActivity.java
+++ ScorePoint/app/src/main/java/com/ipointsystems/scorepoint/DashboardActivity.java
@@ -2,6 +2,10 @@ package com.ipointsystems.scorepoint;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class DashboardActivity extends Activity {
 
@@ -9,6 +13,21 @@ public class DashboardActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        ListView rivalList = (ListView) findViewById(R.id.activity_dashboard_list);
+        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"};
+
+        final ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        rivalList.setAdapter(adapter);
+
     }
 
 
